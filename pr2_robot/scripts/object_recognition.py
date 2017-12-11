17ade75bf3784cc39cf8bb95d88a7558963114c6
carlos <ze_netio@hotmail.com>
2017-12-11 18:15:49 -0200

    Updated README.md

---
+++ pr2_robot/scripts/object_recognition.py
@@ -0,0 +1,360 @@
+#!/usr/bin/env python
+
+# Import modules
+import numpy as np
+import sklearn
+from sklearn.preprocessing import LabelEncoder
+import pickle
+from sensor_stick.srv import GetNormals
+from sensor_stick.features import compute_color_histograms
+from sensor_stick.features import compute_normal_histograms
+from visualization_msgs.msg import Marker
+from sensor_stick.marker_tools import *
+from sensor_stick.msg import DetectedObjectsArray
+from sensor_stick.msg import DetectedObject
+from sensor_stick.pcl_helper import *
+
+import rospy
+import tf
+from geometry_msgs.msg import Pose
+from std_msgs.msg import Float64
+from std_msgs.msg import Int32
+from std_msgs.msg import String
+from pr2_robot.srv import *
+from rospy_message_converter import message_converter
+import yaml
+
+
+# Helper function to get surface normals
+def get_normals(cloud):
+    get_normals_prox = rospy.ServiceProxy('/feature_extractor/get_normals', GetNormals)
+    return get_normals_prox(cloud).cluster
+
+
+# Helper function to create a yaml friendly dictionary from ROS messages
+def make_yaml_dict(test_scene_num, arm_name, object_name, pick_pose, place_pose):
+    yaml_dict = {}
+    yaml_dict["test_scene_num"] = test_scene_num.data
+    yaml_dict["arm_name"] = arm_name.data
+    yaml_dict["object_name"] = object_name.data
+    yaml_dict["pick_pose"] = message_converter.convert_ros_message_to_dictionary(pick_pose)
+    yaml_dict["place_pose"] = message_converter.convert_ros_message_to_dictionary(place_pose)
+    return yaml_dict
+
+
+# Helper function to output to yaml file
+def send_to_yaml(yaml_filename, dict_list):
+    data_dict = {"object_list": dict_list}
+    with open(yaml_filename, 'w') as outfile:
+        yaml.dump(data_dict, outfile, default_flow_style=False)
+
+
+def outlier_filter(pcl_data):
+    # start by creating a filter object to remove noise
+    outlier_filter = pcl_data.make_statistical_outlier_filter()
+    # set the number of neighboring points to analyze for any given point
+    outlier_filter.set_mean_k(50)
+    # any point with a mean distance larger than global (mean distance+x*std_dev)
+    # will be considered outlier
+    outlier_filter.set_std_dev_mul_thresh(0.5)
+    # finally call the filter function for magic
+    pcl_objects = outlier_filter.filter()
+    pcl.save(pcl_objects, 'cloud_filtered1.pcd')
+    outlier_filter.set_negative(True)
+    pcl.save(outlier_filter.filter(), 'cloud_noise1.pcd')
+    return pcl_objects
+
+
+# Callback function for your Point Cloud Subscriber
+def pcl_callback(pcl_msg):
+    # Exercise-2 TODOs:
+    rospy.loginfo('Ex-2')
+    # TODO: Convert ROS msg to PCL data
+    pcl_data = ros_to_pcl(pcl_msg)  # XYZRGB
+    #print('size1: ', pcl_data.size)
+    pcl_data.to_file('pcl_data.pcd')
+    pcl_data = outlier_filter(pcl_data)
+    #print('size2: ', pcl_data.size)
+
+    #cloud_filtered = pcl_data
+
+    # TODO: Voxel Grid Downsampling
+    vox = pcl_data.make_voxel_grid_filter()
+    # try a voxel size and set to cloud
+    LEAF_SIZE = 0.005
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+    # call filter function to obtain the resultant downsampled PC
+    cloud_filtered = vox.filter()
+    # save file
+    ##filename = 'voxel_sampled1.pcd'
+    ##pcl.save(cloud_filtered, filename)
+    #print('size3: ', cloud_filtered.size)
+
+    # TODO: PassThrough Filter to isolate the table and objects
+    passthrough_z = cloud_filtered.make_passthrough_filter()
+    # Assign axis and range to the passthrough filter object
+    filter_axis = "z"
+    passthrough_z.set_filter_field_name(filter_axis)
+    axis_min = 0.6  # 0.6, 1.1
+    axis_max = 0.85 # 1.5  1.1 
+    passthrough_z.set_filter_limits(axis_min, axis_max)
+    #passthrough.set_filter_limits(0, 1.5)
+    # use filter function to obtain only table and objects
+    cloud_filtered = passthrough_z.filter()
+    passthrough_y = cloud_filtered.make_passthrough_filter()
+    passthrough_y.set_filter_field_name("y")
+    axis_min = -0.42
+    axis_max = 0.42
+    passthrough_y.set_filter_limits(axis_min, axis_max)
+    # use filter function to obtain only table and objects
+    cloud_filtered = passthrough_y.filter()
+    # save result
+    filename = 'pass_through_filtered1.pcd'
+    pcl.save(cloud_filtered, filename)
+    #print('size4: ', cloud_filtered.size)
+
+    # TODO: RANSAC Plane Segmentation, will table and objects
+    # seg = cloud_filtered.make_segmenter_normals(ksearch=50)
+    seg = cloud_filtered.make_segmenter()
+    seg.set_optimize_coefficients(True)
+    # TODO: Extract inliers and outliers
+    # set the model we wish to fit
+    # seg.set_model_type(pcl.SACMODEL_NORMAL_PLANE)
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    # seg.set_max_iterations(100)
+    max_distance = 0.018  # 0.01
+    # set max distance for a point to be considered fitting the model
+    seg.set_distance_threshold(max_distance)
+    # call the segment function to obtain set of inlier indices and model coefficients
+    inliers, coefficients = seg.segment()
+    #print(coefficients)
+    # extracted inliers - table
+    pcl_table = cloud_filtered.extract(inliers, negative=False)
+    # save file table
+    filename = 'pcl_table1.pcd'
+    pcl.save(pcl_table, filename)
+    # extracted outliers - objects
+    pcl_objects = cloud_filtered.extract(inliers, negative=True)
+    # save file objects
+    filename = 'pcl_objects1.pcd'
+    pcl.save(pcl_objects, filename)
+
+    # Outlier Removal Filter
+    # pcl_objects = outlier_filter(pcl_objects)
+
+    # TODO: Euclidean Clustering
+    white_cloud = XYZRGB_to_XYZ(pcl_objects)
+    tree = white_cloud.make_kdtree()
+    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # create a cluster extraction object
+    ec = white_cloud.make_EuclideanClusterExtraction()
+    # Set tolerances for distance threshold
+    # as well as minimum and maximum cluster size (in points)
+    ec.set_ClusterTolerance(0.04)  # .08
+    ec.set_MinClusterSize(30)
+    ec.set_MaxClusterSize(100000)
+    # Search the k-d tree for clusters
+    ec.set_SearchMethod(tree)
+    # Extract indices for each of the discovered clusters
+    cluster_indices = ec.Extract()
+
+    # Assign a color corresponding to each segmented object in scene
+    cluster_color = get_color_list(len(cluster_indices))
+    color_cluster_point_list = []
+
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            color_cluster_point_list.append([white_cloud[indice][0],
+                                             white_cloud[indice][1],
+                                             white_cloud[indice][2],
+                                             rgb_to_float(cluster_color[j])])
+
+    # Create new cloud containing all clusters, each with unique color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
+    # TODO: Convert PCL data to ROS messages
+    ros_cloud_objects = pcl_to_ros(pcl_objects)
+    ros_cloud_table = pcl_to_ros(pcl_table)
+    # now publish ros_cluster_cloud
+    ros_cluster_cloud = pcl_to_ros(cluster_cloud)
+
+    # TODO: Publish ROS messages
+    pcl_objects_pub.publish(ros_cloud_objects)
+    pcl_table_pub.publish(ros_cloud_table)
+    pcl_cluster_pub.publish(ros_cluster_cloud)
+
+# Exercise-3 TODOs:
+    rospy.loginfo('Ex-3')
+    # Classify the clusters! (loop through each detected cluster one at a time)
+    detected_objects_list = []
+    detected_labels = []
+
+    for index, pts_list in enumerate(cluster_indices):
+        features = []
+        #print(index, len(cluster_indices))
+        # Grab the points for the cluster
+        pcl_cluster = pcl_objects.extract(pts_list)
+        pcl_cluster.to_file('pcl_cluster1-{0}.pcd'.format(index))
+
+        # Convert the cluster from pcl to ROS using helper function
+        ros_cluster_cloud = pcl_to_ros(pcl_cluster)
+        # Compute the associated feature vector
+        # Extract histogram features
+        chists = compute_color_histograms(ros_cluster_cloud, using_hsv=True)
+        normals = get_normals(ros_cluster_cloud)
+        nhists = compute_normal_histograms(normals)
+        features = np.concatenate((chists, nhists))
+        ###labeled_features.append([feature, model_name])
+        # Make the prediction, retrieve the label for the result
+        # and add it to detected_objects_labels list
+        sc = scaler.transform(features.reshape(1, -1))
+        prediction = clf.predict(sc)
+        label = encoder.inverse_transform(prediction)[0]
+        detected_labels.append(label)
+
+        # Publish a label into RViz
+        label_pos = list(white_cloud[pts_list[0]])
+        label_pos[2] += .4
+        object_markers_pub.publish(make_label(label, label_pos, index))
+
+        # Add the detected object to the list of detected objects.
+        do = DetectedObject()
+        do.label = label
+        do.cloud = ros_cluster_cloud
+        detected_objects_list.append(do)
+
+    # Objects detected
+    rospy.loginfo('Detected {} objects: {}'.format(len(detected_labels), detected_labels))
+
+    # Publish the list of detected objects
+    # This is the output you'll need to complete the upcoming project!
+    detected_objects_pub.publish(detected_objects_list)
+
+    # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
+    # Could add some logic to determine whether or not your object detections are robust
+    # before calling pr2_mover()
+    
+    try:
+        pr2_mover(detected_objects_list)
+    except rospy.ROSInterruptException:
+        pass
+
+# grab object info
+def grab_obj_info(object_list):
+    labels = []
+    centroids = []
+    for obj in object_list:
+        labels.append(obj.label)
+        # TODO: Get the PointCloud for a given object and obtain it's centroid
+        points_arr = ros_to_pcl(obj.cloud).to_array()
+        # TODO: Create 'place_pose' for the object
+        centroids.append(np.mean(points_arr, axis=0)[:3])
+    return labels, centroids
+
+
+# function to load parameters and request PickPlace service
+def pr2_mover(object_list):
+    # TODO: Initialize variables
+    labels = []
+    centroids = []
+    centroid = []
+    object_list_param = []
+    object_name = String()
+    object_group = String()
+    dict_list = []
+    yaml_dict = []
+    # TODO: Get/Read parameters
+    object_list_param = rospy.get_param('/object_list')
+    labels, centroids = grab_obj_info(object_list)
+    # TODO: Parse parameters into individual variables
+    for i in range(0, len(object_list_param)):
+        object_name.data = object_list_param[i]['name']
+        object_group.data = object_list_param[i]['group']
+        #print(i, object_name.data, object_group.data, centroids, labels)
+        # check if is a valid param
+        if object_name.data not in labels:
+            rospy.loginfo("Object {0} not detected in scene!".format(object_name.data))
+            continue
+            # TODO: Rotate PR2 in place to capture side tables for the collision map
+
+            # TODO: Loop through the pick list
+        # correlate the name in list with object detected to find centroid
+        label = None
+        centroid = None
+        for j in range(0, len(labels)):
+            if object_name.data == labels[j]:
+                label = labels[j]
+                centroid = centroids[j]
+                break
+
+        # Initialize variables
+        test_scene_num = Int32()
+        arm_name = String()
+        pick_pose = Pose()
+        place_pose = Pose()
+
+        # TODO: Assign the arm to be used for pick_place
+        # green group(box): right arm, red group(box): left arm
+        test_scene_num.data = 1
+        pick_pose.position.x = float(centroid[0])
+        pick_pose.position.y = float(centroid[1])
+        pick_pose.position.z = float(centroid[2]) - 0.1
+
+        dropbox_list_param = rospy.get_param('/dropbox')
+        for j in range(0, len(dropbox_list_param)):
+            if object_group.data == dropbox_list_param[j]['group']:
+                arm_name.data = dropbox_list_param[j]['name']
+                place_pose.position.x = float(dropbox_list_param[j]['position'][0])
+                place_pose.position.y = float(dropbox_list_param[j]['position'][1])
+                place_pose.position.z = float(dropbox_list_param[j]['position'][2])
+                break
+        print(object_name)
+        # TODO: Create a list of dictionaries (made with make_yaml_dict()) for later output to yaml format
+        yaml_dict = make_yaml_dict(test_scene_num, arm_name, object_name, pick_pose, place_pose)
+        dict_list.append(yaml_dict)
+        # Wait for 'pick_place_routine' service to come up
+        rospy.wait_for_service('pick_place_routine')
+
+        try:
+            pick_place_routine = rospy.ServiceProxy('pick_place_routine', PickPlace)
+
+            # TODO: Insert your message variables to be sent as a service request
+            resp = pick_place_routine(test_scene_num, object_name, arm_name, pick_pose, place_pose)
+
+            print ("Response: ", resp.success)
+
+        except rospy.ServiceException, e:
+            print "Service call failed: %s" % e
+
+    # TODO: Output your request parameters into output yaml file
+    yaml_filename = 'object_list.yaml'
+    send_to_yaml("object_{0}.yaml".format(test_scene_num.data), dict_list)
+
+
+if __name__ == '__main__':
+
+    # TODO: ROS node initialization
+    rospy.init_node('clustering', anonymous=True)
+    # TODO: Create Subscribers
+    pcl_sub = rospy.Subscriber("/pr2/world/points", pc2.PointCloud2, pcl_callback, queue_size=10)
+    # TODO: Create Publishers
+    pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
+    pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
+    pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
+    object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=1)
+    detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
+
+    # TODO: Load Model From disk
+    model = pickle.load(open('model.sav', 'rb'))
+    clf = model['classifier']
+    encoder = LabelEncoder()
+    encoder.classes_ = model['classes']
+    scaler = model['scaler']
+
+    # Initialize color_list
+    get_color_list.color_list = []
+
+    # TODO: Spin while node is not shutdown
+    while not rospy.is_shutdown():
+        rospy.spin()
--- submit/Writeup_Pick_Place_3D.md
+++ submit/Writeup_Pick_Place_3D.md
@@ -1,79 +1,120 @@
 ## Project: Perception Pick & Place
 ### Overview: 
-The target of this project is to provide a Pick & Place solution for a PR2 robot running on Gazebo simulator over ROS. The robot, that is provided with RGB vision camera to allow identify each objectis, pick up a set of different objects and then place in two different boxes. 
-
----
-
-The ROS environment has a set of 2 different files: `pick_list_*.yaml` in `/pr2_robot/config` for a list of models to be identified and `test_*.world` in `/pr2_robot/worlds` to provide different scenerios.
-
-To identify the objects, a serie of computer vision algorithms were used, where object features were extracted and trained an SVM model on new objects, as follow:
-
-
-Features in Training Set: 800
-Invalid Features in Training set: 0
-Scores: [0.94375 0.89375 0.94375 0.95 0.95625]
-Accuracy: 0.94 (+/- 0.04)
-accuracy score: 0.9375
-
-
-
-2. Write a ROS node and subscribe to `/pr2/world/points` topic. This topic contains noisy point cloud data that you must work with.
-3. Use filtering and RANSAC plane fitting to isolate the objects of interest from the rest of the scene.
-4. Apply Euclidean clustering to create separate clusters for individual items.
-5. Perform object recognition on these objects and assign them labels (markers in RViz).
-6. Calculate the centroid (average in x, y and z) of the set of points belonging to that each object.
-7. Create ROS messages containing the details of each object (name, pick_pose, etc.) and write these messages out to `.yaml` files, one for each of the 3 scenarios (`test1-3.world` in `/pr2_robot/worlds/`).  See the example `output.yaml` for details on what the output should look like.  
-8. Submit a link to your GitHub repo for the project or the Python code for your perception pipeline and your output `.yaml` files (3 `.yaml` files, one for each test world).  You must have correctly identified 100% of objects from `pick_list_1.yaml` for `test1.world`, 80% of items from `pick_list_2.yaml` for `test2.world` and 75% of items from `pick_list_3.yaml` in `test3.world`.
-9. Congratulations!  Your Done!
-
-# Extra Challenges: Complete the Pick & Place
-7. To create a collision map, publish a point cloud to the `/pr2/3d_map/points` topic and make sure you change the `point_cloud_topic` to `/pr2/3d_map/points` in `sensors.yaml` in the `/pr2_robot/config/` directory. This topic is read by Moveit!, which uses this point cloud input to generate a collision map, allowing the robot to plan its trajectory.  Keep in mind that later when you go to pick up an object, you must first remove it from this point cloud so it is removed from the collision map!
-8. Rotate the robot to generate collision map of table sides. This can be accomplished by publishing joint angle value(in radians) to `/pr2/world_joint_controller/command`
-9. Rotate the robot back to its original state.
-10. Create a ROS Client for the “pick_place_routine” rosservice.  In the required steps above, you already created the messages you need to use this service. Checkout the [PickPlace.srv](https://github.com/udacity/RoboND-Perception-Project/tree/master/pr2_robot/srv) file to find out what arguments you must pass to this service.
-11. If everything was done correctly, when you pass the appropriate messages to the `pick_place_routine` service, the selected arm will perform pick and place operation and display trajectory in the RViz window
-12. Place all the objects from your pick list in their respective dropoff box and you have completed the challenge!
-13. Looking for a bigger challenge?  Load up the `challenge.world` scenario and see if you can get your perception pipeline working there!
-
-## [Rubric](https://review.udacity.com/#!/rubrics/1067/view) Points
-### Here I will consider the rubric points individually and describe how I addressed each point in my implementation.  
-
----
-### Writeup / README
-
-#### 1. Provide a Writeup / README that includes all the rubric points and how you addressed each one.  You can submit your writeup as markdown or pdf.  
-
-You're reading it!
-
-### Exercise 1, 2 and 3 pipeline implemented
-#### 1. Complete Exercise 1 steps. Pipeline for filtering and RANSAC plane fitting implemented.
-
-#### 2. Complete Exercise 2 steps: Pipeline including clustering for segmentation implemented.  
-
-#### 2. Complete Exercise 3 Steps.  Features extracted and SVM trained.  Object recognition implemented.
-Here is an example of how to include an image in your writeup.
+The target of this project is to provide a Pick & Place solution for a PR2 robot running on Gazebo simulator over ROS. The robot, that is provided with RGB vision camera to allow identify a list of objects using a trained model, pick up a set of different objects and then place in two different boxes. 
 
 ![demo-1](https://user-images.githubusercontent.com/20687560/28748231-46b5b912-7467-11e7-8778-3095172b7b19.png)
+---
+The ROS environment has a set of 2 different configuration files: `pick_list_*.yaml` in `/pr2_robot/config` for a list of models to be identified and `test_*.world` in `/pr2_robot/worlds` to provide different scenerios.
 
+1. To identify the objects, a serie of computer vision algorithms were used, where object features were extracted and trained a SVM model to detect objects, as follow:
 
+- Features in Training Set: 800
+- Invalid Features in Training set: 0
+- Scores: [0.94375 0.89375 0.94375 0.95 0.95625]
+- Accuracy: 0.94 (+/- 0.04)
+- accuracy score: 0.9375
 
+Running the `capture_feature.py` file, 50 features were captured for each object, totalizing 800 features. In sequence, the generated features file is used by `train_svm.py` file to train the object detection model. The figure below show the resulting prediction for the model.
 
-Here's | A | Snappy | Table
---- | --- | --- | ---
-1 | `highlight` | **bold** | 7.41
-2 | a | b | c
-3 | *italic* | text | 403
-4 | 2 | 3 | abcd
+![Fig-1](matrix.JPG)
 
+Note that only two objects are below 0.90 that shows a good result.
+Then we can use the `model.sav` trained file to recognize the objectes detect and manipulated by PR2 robot.
 
-### Pick and Place Setup
+2. Write a ROS node and subscribe to `/pr2/world/points` topic. 
 
-#### 1. For all three tabletop setups (`test*.world`), perform object recognition, then read in respective pick list (`pick_list_*.yaml`). Next construct the messages that would comprise a valid `PickPlace` request output them to `.yaml` format.
+```Python
+# Create Subscribers
+pcl_sub = rospy.Subscriber("/pr2/world/points", pc2.PointCloud2, cl_callback, queue_size=1)
+```
+3. Use filtering and RANSAC plane fitting to isolate the objects of interest from the rest of the scene.
+```Python
+# RANSAC Plane Segmentation, will table and objects
+seg = cloud_filtered.make_segmenter()
+# Extract inliers and outliers
+# set the model we wish to fit
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+max_distance = 0.05
+# set max distance for a point to be considered fitting the model
+seg.set_distance_threshold(max_distance)
+# call the segment function to obtain set of inlier indices and model coefficients
+inliers, coefficients = seg.segment()
+# extracted inliers -> table
+pcl_table = cloud_filtered.extract(inliers, negative=False)
+# extracted outliers -> objects
+pcl_objects = cloud_filtered.extract(inliers, negative=True)
+```
+4. Now, apply Euclidean clustering to create separated clusters for individual items.
+```Python
+# Euclidean Clustering
+white_cloud = XYZRGB_to_XYZ(pcl_objects)
+tree = white_cloud.make_kdtree()
+# Create Cluster-Mask Point Cloud to visualize each cluster separately
+# create a cluster extraction object
+ec = white_cloud.make_EuclideanClusterExtraction()
+# Set tolerances for distance threshold
+# as well as minimum and maximum cluster size (in points)
+ec.set_ClusterTolerance(0.05)
+ec.set_MinClusterSize(50)
+ec.set_MaxClusterSize(100000)
+# Search the k-d tree for clusters
+ec.set_SearchMethod(tree)
+# Extract indices for each of the discovered clusters
+cluster_indices = ec.Extract()
+```
+5. Perform object recognition on these objects and assign them labels (markers in RViz).
+```Python
+# Publish a label into RViz
+label_pos = list(white_cloud[pts_list[0]])
+label_pos[2] += .4
+object_markers_pub.publish(make_label(label,label_pos, index))
+```
+6. Calculate the centroid (average in x, y and z) of the set of points belonging to that each object.
+```Python
+def grab_obj_info(object_list):
+    labels = []
+    centroids = []
+    for obj in object_list:
+        labels.append(obj.label)
+        # Get the PointCloud for a given object and obtain it's centroid
+        points_arr = ros_to_pcl(obj.cloud).to_array()
+        # Create 'place_pose' for the object
+        centroids.append(np.mean(points_arr, axis=0)[:3])
+    return labels, centroids
+```
+7. Create ROS messages containing the details of each object (name, pick_pose, etc.) and write these messages out to `.yaml` files, one for each of the 3 scenarios (`test1-3.world` in `/pr2_robot/worlds/`).  See the example `output.yaml` for details on what the output should look like.
+```Python
+# Create a list of dictionaries (made with make_yaml_dict()) for later output to yaml format
+yaml_dict = make_yaml_dict(test_scene_num, arm_name, object_name, pick_pose, place_pose)
+dict_list.append(yaml_dict)
+# Wait for 'pick_place_routine' service to come up
+rospy.wait_for_service('pick_place_routine')
+```  
+8. Submit a link to your GitHub repo for the project or the Python code for your perception pipeline and your output `.yaml` files (3 `.yaml` files, one for each test world).  You must have correctly identified 100% of objects from `pick_list_1.yaml` for `test1.world`, 80% of items from `pick_list_2.yaml` for `test2.world` and 75% of items from `pick_list_3.yaml` in `test3.world`.
 
-And here's another image! 
-![demo-2](https://user-images.githubusercontent.com/20687560/28748286-9f65680e-7468-11e7-83dc-f1a32380b89c.png)
+### Putting all together
 
-Spend some time at the end to discuss your code, what techniques you used, what worked and why, where the implementation might fail and how you might improve it if you were going to pursue this project further.  
+Scenario 1. We have 3 objects: biscuits, soap, soap2. The figure shows the model prediction about the objects.
 
+![Fig-2](test_1.JPG)
 
+Scenario 2. We have 5 objects: biscuits, soap, soap2, glue, book. The figure below shows the model prediction for the objects.
 
+![Fig-2](test_2.JPG)
+
+Note that the model failed to recognize the book.
+
+Scenario 3. We have 8 objects: biscuits, soap, soap2, glue, book, sticky_notes, snacks, eraser. The figure below shows the model prediction for the objects.
+
+![Fig-2](test_3.JPG)
+
+The model failed to regocnize the glue, but note that tht glue object is partially oclused by the book, which make things more difficult.
+
+### Conclusions
+
+- The code was devided in sections what did the things easier to understand, implement and test. It was possible test each part individually and at final, add all together.
+- I had big problems with Euclidean Clustering because, depending on how you choose the parameters, you might create a dataset that will cause error when you try to process the prediction. I spent a good time until figure out the correct set of parameters to avoid problems ahead.
+- Another point to take care is that the project can deal with RGB or HSV objects but I got better results using HSV option.
+- For future optimization, we can provide a better trained model, for instance, using more training features.
+- Another suggestion for improvement could be use a Neural Network model trained with the entire set of objects and then use the trained model with the robot.
